0852f510d470b7d28b8c9d27159ea0b1eca43bfe
Kashifainii <fivermaster92@gmail.com>
2022-02-17 01:07:41 -0800

    add values and variables

--- practis 10/script.js
+++ practis 10/script.js
@@ -2,3 +2,10 @@ let kashif = 'hello word';
 if (kashif == 'hello word') alert('pakistan zinda bad')
 
 console.log(40 + 80 - 10);
+console.log('kashif');
+console.log(40);
+let fistName = "kashif"
+console.log(fistName);
+console.log(fistName);
+let Kashiif = 'java';
+console.log(Kashiif);
