d5e07028068cdd609e6759bc905a7474571d1b75
Kashifainii <fivermaster92@gmail.com>
2022-02-18 01:13:29 -0800

    add variavle

--- practis 10/script.js
+++ practis 10/script.js
@@ -1,13 +1,22 @@
 let kashif = 'hello word';
 if (kashif == 'hello word') alert('pakistan zinda bad')
 
-console.log(40 + 80 - 10);
-console.log('kashif');
-console.log(40);
-let fistName = "kashif"
-console.log(fistName);
-console.log(fistName);
+console.log(40 + 80 - 10);// console.log('kashif');
+// console.log(40);
+let firstName = "kashif"
+console.log(firstName);
+firstName = "Rashid"
+console.log(firstName);
 let Kashiif = 'java';
 console.log(Kashiif);
-let firstname = 'jona';
-console.log(firstname);
+// let firstname = 'jona';
+// console.log(firstname);
+let firt_name = "kashif";
+console.log(firt_name);
+let javascriptFun = 'true';
+console.log(true);
+console.log(typeof true);
+console.log(typeof 3);
+console.log(typeof 3.1111);
+console.log(typeof kashif);
+firstName = "jobisgood";
