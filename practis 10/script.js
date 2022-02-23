c2fb37cc2f6edf42680cae79656f563c2c9b8821
Kashifainii <fivermaster92@gmail.com>
2022-02-22 22:31:40 -0800

    add diifernet srings together

--- practis 10/script.js
+++ practis 10/script.js
@@ -68,23 +68,23 @@ console.log(agekashif > ageejaz);
 console.log(agekashif < ageejaz);
 console.log(currentyear - 1993 > currentyear - 1975, 'in this line we explain that mathmatical opraters work before the compersion opreatrs');*/
 //opraters predence 
-const currentyear = 2022;
-const agekashif = currentyear - 1993;
-const ageejaz = currentyear - 1975;
-console.log(currentyear - 1993 > currentyear - 1975);
+//const currentyear = 2022;
+//const agekashif = currentyear - 1993;
+//const ageejaz = currentyear - 1975;
+//console.log(currentyear - 1993 > currentyear - 1975);
 
-let x, y;
-x = y = 25 - 10 - 5;
-console.log(x, y, 'in this line x = y and the solve the minus has fist bcz high precdene');
+//let x, y;
+//x = y = 25 - 10 - 5;
+//console.log(x, y, 'in this line x = y and the solve the minus has fist bcz high precdene');
 //const avreg = agekashif + ageejaz / 2;
 //console.log(agekashif, ageejaz, avreg, 'in this line fiest solve the dive and then add both age ');
-const avreg = (agekashif + ageejaz) / 2
-console.log(agekashif, ageejaz, avreg, 'in this line prenthises first solve the dive and then add both age ');
+//const avreg = (agekashif + ageejaz) / 2
+//console.log(agekashif, ageejaz, avreg, 'in this line prenthises first solve the dive and then add both age ');
 //const masskahif = 78;
 //const hightkashif = 1.69;
 //const masshoukat = 92;
 //const hightshoukat = 1.95;
-const masskahif = 95;
+/*const masskahif = 95;
 const hightkashif = 1.88;
 const masshoukat = 85;
 const hightshoukat = 1.76;
@@ -92,3 +92,12 @@ const BMIkashif = masskahif / hightkashif ** 2
 const BMIshoukat = masshoukat / (hightshoukat * hightshoukat);
 const markBMIHigher = masshoukat < masskahif;
 console.log(BMIkashif, BMIshoukat, markBMIHigher, 'in that we mark hiegr masss');
+*/
+const firstName = 'kashif';
+const lastName = 'abdul rasheed';
+const birthYear = 1993;
+const currentyear = 2022;
+const job = 'develpers';
+const kashif = " i'm" + " " + firstName + "  " + lastName + "and my age is " + (currentyear - birthYear) + ".My job is web " + job;
+console.log(kashif);
+
